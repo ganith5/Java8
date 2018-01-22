1be1afa8d090af08c5ce699a6d0010df1aed05c3
Pavithra <pavithra.sachidananda@gmail.com>
2018-01-21 21:55:18 -0500

    Added swap method as utility method

--- src/com/java8/interviews/sorting/Utility.java
+++ src/com/java8/interviews/sorting/Utility.java
@@ -7,8 +7,15 @@ public class Utility {
 
     public static void print(int[] arr) {
         int n = arr.length;
+        //System.out.print("\n");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + ((i == n - 1) ? "" : " "));
         }
+    }
+
+    public static void swap(int a, int b) {
+        int temp = b;
+        b = a;
+        a = temp;
     }
 }
